f6c3cb781b95c95c9725e137d4ae49fa53de68e2
Christian Barrios <christiandbf@hotmail.com>
2018-08-03 18:13:13 -0500

    DB connection added

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const mongoose = require('mongoose');
 
 const morgan = require('morgan');
 
 const app = express();
 
-// variables
+// Express variables
 app.set('port', process.env.PORT || 3000);
 app.set('db_user', process.env.DB_USER);
 app.set('db_password', process.env.DB_PASSWORD);
 
-// middlewares
+// MongoDB connection
+const uri = `mongodb://${app.get('db_user')}:${app.get('db_password')}@ds125146.mlab.com:25146/foundation-test1`;
+mongoose.connect(uri, { useNewUrlParser: true })
+  .then(() => console.log('DB is connected'))
+  .catch(err => console.error(err.message));
+
+// Middlewares
 app.use(morgan('dev'));
 app.use(express.static('public'));
 
+// Serve API Rest Docs
 app.get('/', (req, res) => res.redirect('/index.html'));
 
-app.listen(3000, () => console.log(`API Rest on port ${app.get('port')}`));
+// Init server
+app.listen(app.get('port'), () => console.log(`API Rest on port ${app.get('port')}`));
